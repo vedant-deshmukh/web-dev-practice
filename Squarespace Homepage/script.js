6483bb2dec5f602e053f67ec7c1a9f6bc22554a3
Vedant Deshmukh <83693267+vedant-deshmukh@users.noreply.github.com>
2023-10-10 16:05:14 +0530

    Add files via upload

---
+++ Squarespace Homepage/script.js
@@ -0,0 +1,36 @@
+var tl = gsap.timeline();
+
+tl.from("#nav img, #nav h3, #nav button", {
+y:-100,
+duration: 1,
+delay : 1,
+opacity: 0,  
+stagger: 0.3,
+});
+
+tl.from("#main h1"
+,{
+    y:100,
+    stagger: 0.3,
+    opacity: 0
+});
+
+tl.from("#main > img",{
+    scale: 0,
+    opacity:0,
+    stagger: 0.3,
+    // duration: 1
+});
+
+tl.from("h5",{
+    scale:0,
+    opacity:0
+});
+
+
+tl.to("h5",{
+    y: 30,
+    duration : 0.7,
+    yoyo: true,
+    repeat: -1
+});
